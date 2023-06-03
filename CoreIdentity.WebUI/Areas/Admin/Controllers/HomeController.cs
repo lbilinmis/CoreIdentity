a05a54ea99b47da2d50ba358bc8dc14ad1364960
lbilinmis <leventamed@gmail.com>
2023-06-03 11:57:47 +0300

    Db de kayıtlı kullanıcılaın listesini bir tablo üzerinde gösterme işlemleri

---
+++ CoreIdentity.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CoreIdentity.WebUI.Areas.Admin.Models;
+using CoreIdentity.WebUI.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreIdentity.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser>_userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+            var userViewModelList = userList.Select(x => new UserViewModel()
+            {
+                Id = x.Id,
+                Email = x.Email,
+                UserName = x.UserName,
+            }).ToList();
+
+            return View(userViewModelList);
+        }
+    }
+}
