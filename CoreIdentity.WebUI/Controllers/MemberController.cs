2cff15819dbdbaa7e433b27732e1afc4514a75a9
lbilinmis <leventamed@gmail.com>
2023-06-03 17:52:01 +0300

    Authorize işlemleri

--- CoreIdentity.WebUI/Controllers/MemberController.cs
+++ CoreIdentity.WebUI/Controllers/MemberController.cs
@@ -1,9 +1,11 @@
 ﻿using CoreIdentity.WebUI.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreIdentity.WebUI.Controllers
 {
+    [Authorize]
     public class MemberController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
@@ -11,6 +13,11 @@ namespace CoreIdentity.WebUI.Controllers
         public MemberController(SignInManager<AppUser> signInManager)
         {
             _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
 
         public async Task<IActionResult> LogOut()
